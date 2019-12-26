9b219c0ed7107e30f2f1180824475f394cc625af
Gi-Yong <59243156+Twave1717@users.noreply.github.com>
2019-12-27 01:25:18 +0900

    Add files via upload

---
+++ MyCalculator/src/MyCalculator/MyCalculator.java
@@ -0,0 +1,13 @@
+package MyCalculator;
+
+public class MyCalculator {
+	public static void main(String[] args) {
+		
+		int num = 10;
+		char isEven;
+		isEven = ((num % 4) == 2) ? '2' : 'F';
+		System.out.println(isEven);
+		
+	}
+
+}
